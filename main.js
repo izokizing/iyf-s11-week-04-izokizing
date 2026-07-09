715b18d6b44f2538aa231f2d303304d1ae193367
izokizing <izokizing@gmail.com>
2026-07-09 15:44:42 +0300

    Add main.js with variable declarations and logging

---
+++ main.js
@@ -0,0 +1,9 @@
+let name = "isaac ndegwa";
+let age = 21;
+let isStudent = true;
+
+const date= new Date(9 july 2026);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
