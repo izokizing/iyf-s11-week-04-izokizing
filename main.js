9331d3154db4dc32feada3bef67adb0daa306b0c
izokizing <izokizing@gmail.com>
2026-07-09 16:19:30 +0300

    Fix date initialization syntax in main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  let name = "isaac ndegwa";
  let age = 21;
  let isStudent = true;
- const date= new Date(9 july 2026);
+ const date= new Date("9 july 2026");
 
  console.log(typeof name);
  console.log(typeof age);
